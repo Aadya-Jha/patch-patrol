c50874451d9a12a650a3386fe82c6548a530840d
sanskriti2004 <sanskritigogoi2004@gmail.com>
2026-05-14 12:59:56 +0530

    lint: add eslint config

---
+++ server/eslint.config.js
@@ -0,0 +1,22 @@
+import globals from "globals";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_"
+      }],
+      "no-console": "off",
+    },
+  },
+];
